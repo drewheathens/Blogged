45887eb916b86f6cb9ea62f57ebcf67a46ea1447
drewheathens <mudavadie@gmail.com>
2019-02-19 13:54:32 +0300

    modified manage

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 from app.models import User,Article,Comment
 
-app = create_app('production')
+app = create_app('developement')
 
 manager = Manager(app)
 manager.add_command('server',Server)
