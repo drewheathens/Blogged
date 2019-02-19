0ad874cd4bab52e038554329eb4ddde9009b3305
drewheathens <mudavadie@gmail.com>
2019-02-19 15:10:48 +0300

    changes modified in Blogged

--- app/models.py
+++ app/models.py
@@ -39,8 +39,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(130))
     Article = db.relationship('Article', backref='author', lazy='dynamic')
-    bio = db.Column(db.String(255))
-    profile_pic = db.Column(db.String(255))
+    # bio = db.Column(db.String(255))
+    # profile_pic = db.Column(db.String(255))
     Article = db.relationship('Article',backref = 'user',lazy = "dynamic")
     comment = db.relationship('Comment', backref='user', lazy="dynamic")
 
