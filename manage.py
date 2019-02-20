abfb8b5aee496cdfb9d3b443c76385747e8665b9
drewheathens <mudavadie@gmail.com>
2019-02-20 05:07:51 +0300

    changed imports

--- manage.py
+++ manage.py
@@ -1,30 +1,27 @@
-from app import *
-from flask_script import Manager,Server
+from app import create_app,db
+from flask_script import Manager, Server
 from flask_migrate import Migrate,MigrateCommand
-from app.models import User,Article,Comment
+from app.models import User,Blog,Comments
+app = create_app('development')
 
-app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 '''
-adds the db instance and models to the shell session
+adds the databse instance and models to the shell session
 '''
-
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Article = Article, Comment = Comment)
+    return dict(app = app, db = db, User = User, Blog =Blog,Comments=Comments )
+    
 if __name__ == '__main__':
     manager.run()
