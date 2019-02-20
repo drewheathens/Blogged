297c4df58cde1d29bc89b9f8001a67454a1d4a0c
drewheathens <mudavadie@gmail.com>
2019-02-20 05:12:11 +0300

    added blogs

--- app/main/views.py
+++ app/main/views.py
@@ -1,81 +1,152 @@
 from flask import render_template,request,flash,redirect,url_for
 from . import main
 from flask_login import login_required,current_user
-from app.models import User,Article,Comment
+from app.models import User,Blog,Comments
 from datetime import datetime
-from app import *
-from .forms import *
+from app import db, photos
+from .forms import PostForm,CommentForm
+
+@main.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @main.route('/')
-#login required
+# @login_required
 def index():
-   
-    return render_template('index.html')
 
-@main.route('/', methods=['GET','POST'])
+
+    tech = Blog.query.filter_by(category='Technology').all()
+
+    return render_template('index.html', tech=tech)
+
+
+
+
+@main.route('/index', methods=['GET', 'POST'])
 def home():
-    form = posts()
-
+    form = PostForm()
     if form.validate_on_submit():
-        post = article(body=form.post.data, author = current_user, category = form.category.data)
+        post = Blog(body=form.post.data, author=current_user, category=form.category.data)
         db.session.add(post)
         db.session.commit()
-        flash('posted live!')
+        flash('Your post is now live!')
         return redirect(url_for('main.index'))
 
-    posts = Article.retrieve_posts(id).all()
-    return render_template("posts.html", title="home page", form = form, posts = posts)
+    posts = Blog.retrieve_posts(id).all()
 
+    return render_template("posts.html", title='Home Page', form=form,posts=posts)
 
-@main.route('/posts',methods=['GET', 'POST'])
+@main.route('/post', methods = ['GET','POST'])
 @login_required
 def post():
-   form = Post()
+   form = PostForm()
    if form.validate_on_submit():
        post = form.post.data
        category = form.category.data
        user = current_user
 
 
-       new_Article = Article(body = post,category = category,user = user)
+       new_blog = Blog(body = post,category = category,user = user)
 
-       # save article
-       db.session.add(new_article)
+       # save Blog
+       db.session.add(new_blog)
        db.session.commit()
 
        return redirect(url_for('main.explore',uname = user.username))
 
-   return render_template('posts.html',form = form)
+   return render_template('post.html',form = form)
+
+
+@main.route('/post/<int:id>', methods = ['GET','POST'])
+@login_required
+def user_post(id):
+
+    users_post = Blog.query.filter_by(user_id=id).all()
+    return render_template('user_posts.html',users_post = users_post)
+
+@main.route('/technology' ,methods = ['GET','POST'])
+def technology():
+    technology = Blog.query.filter_by(category = 'Technology').all()
+    form = CommentForm()
+    if form.validate_on_submit():
+        details = form.details.data
+        user = current_user
+
+        new_comment = Comments(details = details,blogs_id=id,user =user)
+        # # save comment
+        db.session.add(new_comment)
+        db.session.commit()
+
+    return render_template('technology.html', technology = technology,form=form)
+
+@main.route('/sales' ,methods = ['GET','POST'])
+def technolog():
+    sales = Blog.query.filter_by(category = 'sales').all()
+    form = CommentForm()
+    if form.validate_on_submit():
+        details = form.details.data
+        user = current_user
+
+        new_comment = Comments(details = details,blogs_id=id,user =user)
+        # # save comment
+        db.session.add(new_comment)
+        db.session.commit()
+
+    return render_template('sales.html', technology = technology,form=form)
+
+
+    
+@main.route('/explore')
+@login_required
+def explore():
+    posts = Blog.query.order_by(Blog.timestamp.desc()).all()
+    return render_template('posts.html', title='Explore', posts=posts)
+
+
+@main.route('/comments/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_comment(id):
+    comment = Comments.query.filter_by(blogs_id=id).all()
+
+    form_comment = CommentForm()
+    if form_comment.validate_on_submit():
+        details = form_comment.details.data
+
+        new_comment = Comments(details = details,blogs_id=id,user=current_user)
+        # # save comment
+        db.session.add(new_comment)
+        db.session.commit()
+
+    return render_template('comments.html',form_comment = form_comment,comment=comment)
+@main.route('/user/<username>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(username):
+    user = User.query.filter_by(username = username).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('auth.user_profile',username=username))    
+
 
 @main.route('/user/<username>')
 @login_required
 def user_profile(username):
-
     user = User.query.filter_by(username=username).first_or_404()
-    posts= [
-    {
-    'author':user, 'body':'test Posts#1'
-    }
-
+    posts = [
+        {
+            'author':user, 'body':'test Post#1'
+        }
     ]
     return render_template('profile/user_profile.html',posts=posts, user=user)
-
-
-# @main.route('/comments',methods=['GET'],['POST'])
-
-# @main.route('/post/new', methods=['GET,[POST'])
-# def new_post()
-#     form = posts()
-#     if form.validate_on_submit():
-#        posted = form.content.data
-#        new_post = Article(posted=posted, category=form.category.data, user=current_user)
-#        new_post=save_Article()
-#        return redirect(url_for('main.view_post'))
-#     return render_template('posts.html', form=form)
-
-# @main.route('article/new/view')
-# def view_post():
-#     post = Article.query.filter_by(category='Article')
+    '''
+    i have used a variant of first() called fist_or_404()
+    which works exactly like first() when there are results, and in case there 
+    are no results it auto sends a 404 error back
+    '''
 
 
 
